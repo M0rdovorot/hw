b2bff80c3c73995c8a11d22a7aaf542dd9106802
M0rdovorot <sera.ill@yandex.ru>
2023-04-05 14:27:40 +0300

    divided fucntion

--- include/parsing.hpp
+++ include/parsing.hpp
@@ -16,6 +16,17 @@
 #define OPERATION_ABS_PRIORITY 3
 #define OPERATION_UNARY_MINUS_PRIORITY 3
 
+static std::map<std::string, int> operation_priority = {
+  {"(", OPERATION_L_BRACKET_PRIORITY},
+  {"+", OPERATION_PLUS_PRIORITY}, 
+  {"-", OPERATION_MINUS_PRIORITY}, 
+  {"/", OPERATION_DIVIDE_PRIORITY}, 
+  {"arctg", OPERATION_ARCTG_PRIORITY},
+  {"abs", OPERATION_ABS_PRIORITY},
+  {"~", OPERATION_UNARY_MINUS_PRIORITY}
+};
+
 std::string ConvertToPostfix(std::string);
 std::unique_ptr<ICalculatable> MakeTree(std::istream& expression);
 bool Help(int argc, char* argv[]);
+void CheckAndPushOperation(std::string& infix, std::string& postfix, int& pos, std::stack<std::string>& operation_stack);
--- src/parsing.cpp
+++ src/parsing.cpp
@@ -2,16 +2,51 @@
 #include "interface.hpp"
 #include "operations.hpp"
 
+void CheckAndPushOperation(std::string& infix, std::string& postfix, int& pos, std::stack<std::string>& operation_stack){
+  if (pos < infix.size()) {
+    switch (infix[pos]) {
+      case '(':
+        operation_stack.push("(");
+        pos++;
+        break;
+
+      case ')':
+        while (!operation_stack.empty() && operation_stack.top() != "(") {
+          postfix += operation_stack.top() + " ";
+          operation_stack.pop();
+        }
+
+        if (operation_stack.empty()) throw "Wrong expression";
+        if (operation_stack.top() == "(") operation_stack.pop();
+        pos++;
+        break;
+
+      case ' ':
+        while (infix[pos] == ' ') {
+          pos++;
+        }
+        break;
+
+      default:
+        std::string tmp;
+        tmp = infix[pos];
+        if (infix[pos] == '-' && (pos < 2 || infix[pos - 1] == '(' ||  infix[pos - 1] == ' ')) {
+          tmp = '~';
+        }
+        if (operation_priority.find(tmp) != operation_priority.end()) {
+          if (!operation_stack.empty() && (operation_priority[operation_stack.top()] >= operation_priority[tmp])) {
+            postfix += operation_stack.top() + " ";
+            operation_stack.pop();
+          }
+          operation_stack.push(tmp);
+          pos++;
+        }
+    }
+  }  
+}
+
 std::string ConvertToPostfix(std::string infix) {
-  std::map<std::string, int> operation_priority = {
-    {"(", OPERATION_L_BRACKET_PRIORITY},
-    {"+", OPERATION_PLUS_PRIORITY}, 
-    {"-", OPERATION_MINUS_PRIORITY}, 
-    {"/", OPERATION_DIVIDE_PRIORITY}, 
-    {"arctg", OPERATION_ARCTG_PRIORITY},
-    {"abs", OPERATION_ABS_PRIORITY},
-    {"~", OPERATION_UNARY_MINUS_PRIORITY}
-  };
+
   std::stack<std::string> operation_stack;
   std::string postfix = "";
   int pos = 0;
@@ -36,46 +71,7 @@ std::string ConvertToPostfix(std::string infix) {
       operation_stack.push(tmp);
       tmp = "";
     }
-
-    if (pos < infix.size()) {
-      switch (infix[pos]) {
-        case '(':
-          operation_stack.push("(");
-          pos++;
-          break;
-
-        case ')':
-          while (!operation_stack.empty() && operation_stack.top() != "(") {
-            postfix += operation_stack.top() + " ";
-            operation_stack.pop();
-          }
-
-          if (operation_stack.empty()) throw "Wrong expression";
-          if (operation_stack.top() == "(") operation_stack.pop();
-          pos++;
-          break;
-
-        case ' ':
-          while (infix[pos] == ' ') {
-            pos++;
-          }
-          break;
-
-        default:
-          tmp = infix[pos];
-          if (infix[pos] == '-' && (pos < 2 || infix[pos - 1] == '(' ||  infix[pos - 1] == ' ')) {
-            tmp = '~';
-          }
-          if (operation_priority.find(tmp) != operation_priority.end()) {
-            if (!operation_stack.empty() && (operation_priority[operation_stack.top()] >= operation_priority[tmp])) {
-              postfix += operation_stack.top() + " ";
-              operation_stack.pop();
-            }
-            operation_stack.push(tmp);
-            pos++;
-          }
-      }
-    }
+    CheckAndPushOperation(infix, postfix, pos, operation_stack);
   }
   while (!operation_stack.empty()) {
     postfix += operation_stack.top() + " ";
